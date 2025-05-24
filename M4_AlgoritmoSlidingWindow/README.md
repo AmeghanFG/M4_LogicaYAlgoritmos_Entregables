deb22a9b9c3d1d05dbfbe581cce82dc31303a636
Meghan <afigueroa57@ucol.mx>
2025-05-24 15:35:48 -0600

    Terminar primera versión funcinal de Algoritmo Sliding Window y agregando README

---
+++ M4_AlgoritmoSlidingWindow/README.md
@@ -0,0 +1,19 @@
+#  Problema: Encontrar la Palabra Más Larga
+## 📋 Instrucciones
+- Divide el texto en palabras individuales utilizando el método .split(' ') en la cadena.
+- Desliza una ventana que recorra cada palabra del arreglo y compara su longitud con la palabra más larga encontrada hasta ese momento.
+- Devuelve la palabra más larga al final del proceso.
+- 
+## 🧩 Pistas 
+- Usa el método .split(' ') para convertir el texto en un arreglo de palabras separadas por espacios.
+- Compara las longitudes de las palabras utilizando la propiedad length.
+- Guarda la palabra más larga encontrada hasta el momento en la variable longestWord.
+
+## Autor(es)
+- AMFG&P [@AmeghanFG](https://github.com/AmeghanFG) 
+
+## Modulo 4
+Lógica y Algoritmos
+
+## Lección 5
+Algoritmo Sliding Window
---
+++ M4_AlgoritmoSlidingWindow/script.js
@@ -0,0 +1,21 @@
+function findLongestWord(text) {
+    // TODO: Dividir el texto en palabras y almacenarlas en una variable
+    const words = text.split(' '); // Separación de las palabras mediante split 
+
+    let longestWord = ''; // Inicializar la palabra más larga
+
+     // TODO: Recorrer el arreglo de palabras con un ciclo
+    for (let i = 0; i < words.length; i++) {
+         // TODO: Comparar la longitud de la palabra actual con la más larga
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i]; // Actualizar la palabra más larga
+        }
+    }
+
+    // TODO: Retornar la palabra más larga encontrada
+    return longestWord;
+}
+
+// Ejemplo de uso
+const text = "JavaScript es un lenguaje de programación increíble para aprender.";
+console.log(findLongestWord(text)); // Resultado esperado: "programación"
